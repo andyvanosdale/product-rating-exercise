70e1837538b7952f95429a7b8763805152ac8651
Andy Vanosdale <andy@vanosdale.com>
2022-06-24 02:42:01 +0300

    Allow for focusable tests (fit, focus)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,4 +48,9 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+end
+
+RSpec.configure do |config|
+  config.filter_run_when_matching focus: true
 end
